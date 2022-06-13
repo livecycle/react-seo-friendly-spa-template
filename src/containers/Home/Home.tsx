71fc8e5d3ecdfcb9207d675445e4cc0b5579e828
naorzr <naorzruk@gmail.com>
2022-06-13 14:17:14 +0300

    Update Home.tsx

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -37,18 +37,10 @@ const Home: FunctionComponent = () => {
             <p className='title'>react-helmet</p>
             <div className='content'>
               <p>
-                <code>react-helmet</code> is a React plugin that allows you to
-                manage your app's meta information, much like vue-meta does for
-                Vue (vue-meta is based on react-helmet). It makes use of a
-                reusable component that takes plain HTML tags and outputs plan
-                HTML tags.
+                <code>react-helmet</code> 3
               </p>
               <p>
-                These properties, when set on a deeply nested component, will
-                cleverly overwrite their parent components' meta tag information,
-                thereby enabling custom info for each top-level view as well as
-                coupling meta info directly to deeply nested subcomponents for
-                more maintainable code.
+                3
               </p>
             </div>
           </div>
@@ -57,18 +49,10 @@ const Home: FunctionComponent = () => {
             <p className='title'>react-ga</p>
             <div className='content'>
               <p>
-                <code>react-ga</code> is a JavaScript module that can be used to
-                include Google Analytics tracking code in a website or app that
-                uses React for its front-end codebase. It does not currently use
-                any React code internally, but has been written for use with a
-                number of Mozilla Foundation websites that are using React, as a
-                way to standardize our GA Instrumentation across projects.
+                <code>react-ga</code> 3
               </p>
               <p>
-                It is designed to work with the latest version of Google
-                Analytics, Universal Analytics. At this point, all Google
-                Analytics projects are being upgraded to Universal Analytics, so
-                this module will not support the older ga.js implementation.
+                3
               </p>
             </div>
           </div>
@@ -82,13 +66,7 @@ const Home: FunctionComponent = () => {
                 App (because it's great)
               </p>
               <p>
-                Prerendering differs from (SSR) Server Side Rendering. You can get
-                almost all the advantages of it (without the disadvantages) by
-                using prerendering. Prerendering is basically firing up a headless
-                browser, loading your app's routes, and saving the results to a
-                static HTML file. You can then serve it with whatever
-                static-file-serving solution you were using previously. It just
-                works with HTML5 navigation and the likes.
+                3
               </p>
             </div>
           </div>
